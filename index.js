9ecdd3e4b7936cb2845f2015e32aff10644b31cd
wineshuga <uzochukwuwinnie@gmail.com>
2023-06-19 14:36:30 +0100

    Add function to index.js file

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const openIcon = document.querySelector('.open-icon');
 const closeIcon = document.querySelector('.cancel-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const closeMenu = () => {
+  mobileMenu.style.display = 'none';
+  openIcon.style.display = 'block';
+};
+
 openIcon.addEventListener('click', () => {
   openIcon.style.display = 'none';
   mobileMenu.style.display = 'block';
 });
-closeIcon.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
-  openIcon.style.display = 'block';
-});
+closeIcon.addEventListener('click', closeMenu);
